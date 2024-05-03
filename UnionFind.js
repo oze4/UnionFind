22b662ca328de1877b74b1eb9aaaadac24018df0
Matt Oestreich <matthewpoestreich@gmail.com>
2024-05-03 15:00:50 -0500

    Update UnionFind.js

--- UnionFind.js
+++ UnionFind.js
@@ -1,4 +1,4 @@
-class UnionFind {
+export default class UnionFind {
     // @param {Number} numberOfItems : number of vertices.
     // @param {Number[][]} edges : Requires 2d array of ints. Must be of following shape:
     //                             [[Number, Number], [Number, Number], [Number, Number]]
@@ -9,9 +9,36 @@ class UnionFind {
         this.size = new Array(numberOfItems).fill(1);
         // Total number of connected nodes.
         this.count = numberOfItems;
+        // Store adjacency list for cycle detection.
+        this.adjacencyList = {};
         if (edges) {
             this.unionAll(edges);
         }
+    }
+
+    #addToAdjacencyList(nodeA, nodeB) {
+        if (!this.adjacencyList[nodeA]) {
+            this.adjacencyList[nodeA] = [];
+        }
+        if (!this.adjacencyList[nodeB]) {
+            this.adjacencyList[nodeB] = [];
+        }
+        this.adjacencyList[nodeA].push(nodeB);
+        this.adjacencyList[nodeB].push(nodeA);
+    }
+
+    // @param {Set} visited
+    #detectCycle(node, parent, visited) {
+        if (visited.has(node)) {
+            return true;
+        }
+        visited.add(node);
+        for (const descendant of this.adjacencyList[node]) {
+            if (descendant !== parent && this.#detectCycle(descendant, node, visited)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     find(node) {
@@ -22,9 +49,11 @@ class UnionFind {
         return node;
     }
 
-    union(nodeA, nodeB) {
-        let rootA = this.find(nodeA);
-        let rootB = this.find(nodeB);
+    union(src, dest) {
+        this.#addToAdjacencyList(src, dest);
+
+        let rootA = this.find(src);
+        let rootB = this.find(dest);
 
         if (rootA === rootB) {
             return;
@@ -52,6 +81,14 @@ class UnionFind {
     hasPath(nodeA, nodeB) {
         return this.find(nodeA) === this.find(nodeB);
     }
-}
 
-module.exports = UnionFind;
+    hasCycle() {
+        const visited = new Set();
+        for (const node in Object.keys(this.adjacencyList)) {
+            if (!visited.has(node) && this.#detectCycle(node, null, visited)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
